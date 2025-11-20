861bbd35b59fe37e0bef26b53dee970ada6c2b61
FelinloloSchool <leanne.roche@monlycee.net>
2025-11-20 14:12:09 +0100

    start

---
+++ bird.py
@@ -0,0 +1,8 @@
+class Bird:
+    def __init__(self, weights : list, biais : list, ):
+        self.w = weights
+        self.b = biais
+
+    def move(self, inputs):
+        # neuronal application
+        pass
